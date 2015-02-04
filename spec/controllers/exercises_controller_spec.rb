39ea240f6f9571f4c6623d010a7f6c321ae10942
B. Ruschill <brandon.ruschill@gmail.com>
2015-02-04 00:09:19 -0800

    add tests for ExercisesController#index and #new

--- spec/controllers/exercises_controller_spec.rb
+++ spec/controllers/exercises_controller_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 describe ExercisesController do
+  context "#index" do
+    before(:each) do
+      get :index
+    end
 
+    it { should render_template(:index) }
+  end
+
+  context "#new" do
+    before(:each) do
+      get :new
+    end
+
+    it { should render_template(:new) }
+  end
 end
