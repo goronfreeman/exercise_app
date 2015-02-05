dfcd94625316c86efd0ab4c7d60c33fa620a52ea
Hunter Braun <hunter.braun@gmail.com>
2015-02-04 22:11:09 -0800

    Add uniqueness validation & per user index

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,7 @@
 class ExercisesController < ApplicationController
 
   def index
-    @exercises = Exercise.all
+    @exercises = current_user.exercises
   end
 
   def show
@@ -9,21 +9,21 @@ class ExercisesController < ApplicationController
   end
 
   def new
-    @exercise = Exercise.new
-  end
-
-  def edit
-    @exercise = Exercise.find(params[:id])
+    @exercise = current_user.exercises.build
   end
 
   def create
-    @exercise = Exercise.new(exercise_params)
+    @exercise = current_user.exercises.build(exercise_params)
 
     if @exercise.save
       redirect_to exercises_path
     else
       render 'new'
     end
+  end
+
+  def edit
+    @exercise = Exercise.find(params[:id])
   end
 
   def update
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ActiveRecord::Base
   belongs_to :user
   has_many :exercise_sessions
-  
-  validates :name, presence: true
+
+  validates :name, presence: true, uniqueness: { scope: :user }
 end
