1228fccec2674798a9b1f9d13b28376119a09d1c
Hunter Braun <hunter.braun@gmail.com>
2015-02-10 21:42:57 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user_attr = {
+  email: "test@test.com",
+  password: "password",
+  password_confirmation: "password"
+}
+
+user = User.new(user_attr)
+user.save!
+
+exercise_attr = {
+  name: "test"
+}
+
+exercise = user.exercises.build(exercise_attr)
+exercise.save!
+
+exercise_session_attr = {
+  set_goal: 3,
+  rep_goal: 3,
+  duration_goal: 1,
+  weight: 5
+}
+
+exercise_session = exercise.exercise_sessions.new(exercise_session_attr)
+exercise_session.save!
