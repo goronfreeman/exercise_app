c08b9644c4ff0ff10edc2a50e5d3cbee2d7eb231
Hunter Braun <hunter.braun@gmail.com>
2015-04-02 21:51:09 -0700

    Remove shallow routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  shallow do
-    resources :exercises do
-      resources :exercise_sessions do
-        resources :exercise_sets
-      end
+  resources :exercises do
+    resources :exercise_sessions do
+      resources :exercise_sets
     end
   end
 end
